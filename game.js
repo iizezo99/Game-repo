1cc0c9a1e8ec54e7146b29fe16d6d772f4764aae
Abdulaziz <nagla76623@gmail.com>
2025-02-15 19:39:25 +0200

    game

---
+++ game.js
@@ -0,0 +1,52 @@
+function initializeGame() {
+  return new Promise((resolve) => {
+    console.log("Initializing game...");
+    setTimeout(() => {
+      console.log("Game initialized successfully!");
+      resolve();
+    }, 2000); 
+  });
+}
+
+function movePlayer(direction) {
+  return new Promise((resolve) => {
+    console.log(`Player moving ${direction}...`);
+    setTimeout(() => {
+      console.log(`Player moved ${direction} successfully!`);
+      resolve();
+    }, 1000); 
+  });
+}
+
+let playerScore = 0;
+function calculateScore(points) {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      playerScore += points;
+      console.log(`Score updated! Current score: ${playerScore}`);
+      resolve(playerScore);
+    }, 500); 
+  });
+}
+
+function checkGameOver() {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      if (playerScore >= 10) {
+        console.log("Game over! You win!");
+        resolve("Game Over");
+      } else {
+        console.log("Continue playing.");
+        resolve("Continue");
+      }
+    }, 500); 
+  });
+}
+
+initializeGame()
+  .then(() => movePlayer("up"))
+  .then(() => calculateScore(5))
+  .then(() => movePlayer("right"))
+  .then(() => calculateScore(5))
+  .then(() => checkGameOver())
+  .catch((error) => console.error("An error occurred:", error));
